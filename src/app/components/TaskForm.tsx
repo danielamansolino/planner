06def6c705f19e34ed8e847f5d10ad4db646f3c0
danielamansolino <danielamosqueragranda@gmail.com>
2023-11-15 21:49:16 -0500

    Tasks add complete

--- src/app/components/TaskForm.tsx
+++ src/app/components/TaskForm.tsx
@@ -1,57 +1,54 @@
-"use client"; 
-
-import React, { useState } from "react"; 
-import { itemSetter } from "../../utilities/localStorage-utility"
+import React, { useState } from "react";
+import { itemSetter } from "../../utilities/localStorage-utility";
 
 interface TaskFormProps {
-    onTaskAdd: (task: string, date: string) => void;
+  onTaskAdd: (task: string, date: string) => void;
 }
 
-const TaskForm: React.FC = () => { // temporarily moved for testing: <TaskFormProps>
-    const [task, setTask] = useState<string>("");
-    const [date, setDate] = useState<string>("");
-    const [complete, setComplete] = useState<boolean>(false);
+const TaskForm: React.FC<TaskFormProps> = ({ onTaskAdd }) => {
+  const [task, setTask] = useState<string>("");
+  const [date, setDate] = useState<string>("");
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        if (task.trim() === "" || date === "" )return;
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (task.trim() === "" || date === "") return;
 
-        const data = {
-            task: task,
-            date: date
-        }
-        
-        itemSetter(0, data)
-       
-        
-        onTaskAdd(task, date);
-        setTask("");
-        setDate("");
-        setComplete(false);
-    }
+    const data = {
+      task: task,
+      date: date,
+    };
 
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <input 
-                className="taskText text-black" 
-                type="text" 
-                value={task}
-                onChange={(e) => setTask(e.target.value)}
-                placeholder="Enter task"
-                /> 
-                <br />
-                <input
-                className="date text-black"
-                type="date"
-                value={date}
-                onChange={(e) => setDate(e.target.value)}
-                placeholder="Select a date"
-                /><br />
-                <button type="submit">Add task</button>
-            </form>
-        </>    
-    );
-}
+    itemSetter(0, data)
+
+    onTaskAdd(task, date);
+    setTask("");
+    setDate("");
+   
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="taskText text-black"
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Enter task"
+        />
+        <br />
+        <input
+          className="date text-black"
+          type="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          placeholder="Select a date"
+        />
+        <br />
+        <button type="submit">Add task</button>
+      </form>
+    </>
+  );
+};
 
 export default TaskForm;
--- src/utilities/localStorage-utility.ts
+++ src/utilities/localStorage-utility.ts
@@ -1,29 +1,29 @@
 interface ObjectType {
-    task: string,
-    date: number
-}
-
-// itemSetter() saves the item to localStorage
-export const itemSetter = (key: string, data: ObjectType) => {
-    try{
-        const serializedData = JSON.stringify(data);
-        localStorage.setItem(key, serializedData)
-
+    task: string;
+    date: string; // or date: Date;
+  }
+  
+  type StorageKey = "tasks" | "user" | "otherKey";
+  
+  export const itemSetter = (key: StorageKey, data: ObjectType) => {
+    try {
+      const serializedData = JSON.stringify(data);
+      localStorage.setItem(key, serializedData);
     } catch (err) {
-        console.error("Error saving to local storage, error")
+      console.error(`Error saving to local storage (${key}):`, err);
     }
-}
-
-export const itemGetter = (key: string) => {
-    try{
-        const serializedData = localStorage.getItem(key);
-        if (serializedData == null){
-            return null
-        }
-        return JSON.parse(serializedData)
-    } catch(err){
-        console.error("Error retrieve data", err)
-        return null
+  };
+  
+  export const itemGetter = (key: StorageKey): ObjectType | null => {
+    try {
+      const serializedData = localStorage.getItem(key);
+      if (serializedData === null) {
+        return null;
+      }
+      return JSON.parse(serializedData);
+    } catch (err) {
+      console.error(`Error retrieving data from local storage (${key}):`, err);
+      return null;
     }
-
-}
+  };
+  
